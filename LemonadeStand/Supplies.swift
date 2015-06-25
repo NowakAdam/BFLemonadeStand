a6e4ebe96915ffca48e55c21b777e6ee8e390f90
Adam Nowak <adamcbf@gmail.com>
2015-06-25 13:44:01 +0200

    Struct + initializer and storyboard added

---
+++ LemonadeStand/Supplies.swift
@@ -0,0 +1,23 @@
+//
+//  Supplies.swift
+//  LemonadeStand
+//
+//  Created by Adam Nowak on 25.06.2015.
+//  Copyright (c) 2015 Nowak Adam. All rights reserved.
+//
+
+import Foundation
+
+struct Supplies {
+    var money = 0
+    var lemons = 0
+    var iceCubes = 0
+    
+    init (aMoney : Int, aLemons: Int, aIceCubes: Int){
+        money = aMoney
+        lemons = aLemons
+        iceCubes = aIceCubes
+    }
+    
+    
+}
