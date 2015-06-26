1a2a02f66504483a6e82a8c29306649282746f34
Adam Nowak <adamcbf@gmail.com>
2015-06-26 11:33:26 +0200

    clear mix and purchase fix

---
+++ LemonadeStand/Price.swift
@@ -0,0 +1,14 @@
+//
+//  Price.swift
+//  LemonadeStand
+//
+//  Created by Adam Nowak on 25.06.2015.
+//  Copyright (c) 2015 Nowak Adam. All rights reserved.
+//
+
+import Foundation
+
+struct Price {
+    let lemon = 2
+    let iceCube = 1
+}
--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -18,9 +18,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var lemonMixCount: UILabel!
     @IBOutlet weak var iceCubeMixCount: UILabel!
 
+    var supplies = Supplies(aMoney: 10, aLemons: 1, aIceCubes: 1)
+    let price = Price()
+    
+    var lemonsToPurchase = 0
+    var iceCubesToPurchase = 0
+    
+    var lemonsToMix = 0
+    var iceCubesToMix = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateMainView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,35 +43,168 @@ class ViewController: UIViewController {
     //IBActions
     
     @IBAction func purchaseLemonButtonPressed(sender: UIButton) {
+        
+        if supplies.money >= price.lemon{
+            lemonsToPurchase += 1
+            supplies.money -= price.lemon
+            supplies.lemons += 1
+            updateMainView()
+        }
+        else {
+            showAlertWithText(message: "You dont have enough money")
+        }
     }
     
     @IBAction func purchaseIceCubeButtonPressed(sender: UIButton) {
+        if supplies.money >= price.iceCube {
+            iceCubesToPurchase += 1
+            supplies.money -= price.iceCube
+            supplies.iceCubes += 1
+            updateMainView()
+        }
+        else {
+            showAlertWithText(header: "Error", message: "You dont have enough money")
+        }
     }
     
     @IBAction func unpurchaseLemonButtonPressed(sender: UIButton) {
+        if lemonsToPurchase > 0 {
+            lemonsToPurchase -= 1
+            supplies.money += price.lemon
+            supplies.lemons -= 1
+            updateMainView()
+        }
+        else {
+            showAlertWithText(header: "Error", message: "You don't have any lemons to return")
+        }
     }
    
     @IBAction func unpurchaseIceCubeButtonPressed(sender: UIButton) {
+        if iceCubesToPurchase > 0 {
+            iceCubesToPurchase -= 1
+            supplies.money += price.iceCube
+            supplies.iceCubes -= 1
+            updateMainView()
+        }
+        else {
+            showAlertWithText(header: "Error", message: "You don't have any ice cubes to return")
+        }
+        
     }
     
     @IBAction func mixLemonButtonPressed(sender: UIButton) {
+        if supplies.lemons > 0 {
+            lemonsToPurchase = 0
+            supplies.lemons -= 1
+            lemonsToMix += 1
+            updateMainView()
+        }
+        else {
+            showAlertWithText(message: "You don't have enough inventory")
+        }
+        
     }
     
     @IBAction func mixIceCubeButtonPressed(sender: UIButton) {
+        if supplies.iceCubes > 0 {
+            iceCubesToPurchase = 0
+            supplies.iceCubes -= 1
+            iceCubesToMix += 1
+            updateMainView()
+        }
+        else {
+            showAlertWithText(message: "You don't have enough inventory")
+        }
     }
     
     @IBAction func unmixLemonButtonPressed(sender: UIButton) {
+        if lemonsToMix > 0 {
+            lemonsToPurchase = 0
+            lemonsToMix -= 1
+            supplies.lemons += 1
+            updateMainView()
+        }
+        else {
+            showAlertWithText(message: "You have nothing to un-mix")
+        }
     }
   
     @IBAction func unmixIceCubeButtonPressed(sender: UIButton) {
+        if iceCubesToMix > 0 {
+            iceCubesToPurchase = 0
+            iceCubesToMix -= 1
+            supplies.iceCubes += 1
+            updateMainView()
+        }
+        else {
+            showAlertWithText(message: "You have nothing to un-mix")
+        }
+        
     }
     
     @IBAction func startDayButtonPressed(sender: UIButton) {
+        
+        let customers = Int(arc4random_uniform(UInt32(11)))
+        println("Customers : \(customers)")
+        
+        if lemonsToMix == 0 || iceCubesToMix == 0 {
+            showAlertWithText(message: "You need to add atleast 1 lemon and 1 Ice Cube")
+        }
+        else {
+            let lemonadeRatio = Double(lemonsToMix) / Double (iceCubesToMix)
+            for x in 0...customers {
+                let preference = Double(arc4random_uniform(UInt32(101))) / 100
+                if preference < 0.4 && lemonadeRatio > 1 {
+                    supplies.money += 1
+                    println("Paid")
+                }
+                else if preference > 0.6 && lemonadeRatio < 1 {
+                        supplies.money += 1
+                    println("Paid")
+                    }
+                    else if preference <= 0.6 && preference >= 0.4 && lemonadeRatio == 1 {
+                    supplies.money += 1
+                    println("Paid")
+                    }
+                else {
+                    println("else statement evaluating")
+                }
+            }
+            lemonsToPurchase = 0
+            iceCubesToPurchase = 0
+            lemonsToMix = 0
+            iceCubesToMix = 0
+            updateMainView()
+        }
+    }
+
+    //Helper Functions
+    
+    func updateMainView () {
+        
+        moneySupplyCount.text = "$\(supplies.money)"
+        lemonSupplyCount.text = "\(supplies.lemons) lemons"
+        iceCubeSupplyCount.text = "\(supplies.iceCubes) Ice Cubes"
+        
+        lemonuPurchaseCount.text = "\(lemonsToPurchase)"
+        iceCubePurchaseCount.text = "\(iceCubesToPurchase)"
+        
+        lemonMixCount.text = "\(lemonsToMix)"
+        iceCubeMixCount.text = "\(iceCubesToMix)"
+        
     }
     
-    
-    
-    
-
-}
+    func showAlertWithText (header: String = "Warning", message: String) {
+        //iOS 8 & x < 8
+        let alert = UIAlertView()
+        alert.title = header
+        alert.message = message
+        alert.addButtonWithTitle("OK")
+        alert.show()
+//for ios 8
+//        var alert = UIAlertController(title: header, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+//        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+//        self.presentViewController(alert, animated: true, completion: nil)
+    }
+  }
 
